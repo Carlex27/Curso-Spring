5a2b0a9e7a09632c39a80cd94e490bf16d7bb255
Carlex27 <carlosocampo2711@gmail.com>
2024-06-08 16:21:49 -0600

    initial commit

---
+++ SpringCurso1/src/main/java/com/example/SpringCurso1/SpringCurso1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringCurso1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCurso1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCurso1Application.class, args);
+	}
+
+}
---
+++ SpringCurso1/src/test/java/com/example/SpringCurso1/SpringCurso1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringCurso1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCurso1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
