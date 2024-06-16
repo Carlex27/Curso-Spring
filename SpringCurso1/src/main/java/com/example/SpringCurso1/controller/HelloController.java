1715d5b7521126e72e4b50bcd92954ead2662929
Carlex27 <carlosocampo2711@gmail.com>
2024-06-16 15:26:50 -0600

    VIDEO 10 TERMINADO CURSO SPRING OB

--- SpringCurso1/src/main/java/com/example/SpringCurso1/controller/HelloController.java
+++ SpringCurso1/src/main/java/com/example/SpringCurso1/controller/HelloController.java
@@ -1,13 +1,17 @@
 package com.example.SpringCurso1.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${app.varexample}")
+    String message;
 
     @GetMapping("/hello")
     public String hello() {
+        System.out.println(message);
         return "Hello World!";
     }
 }
--- SpringCurso1/src/test/java/com/example/SpringCurso1/SpringCurso1ApplicationTests.java
+++ SpringCurso1/src/test/java/com/example/SpringCurso1/SpringCurso1ApplicationTests.java
@@ -3,12 +3,14 @@ package com.example.SpringCurso1;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class SpringCurso1ApplicationTests {
 
 	@Test
 	void contextLoads() {
-
+		System.getenv().forEach(
+				(key, value) -> System.out.println(key + ":" + value)
+		);
 	}
 
 }
