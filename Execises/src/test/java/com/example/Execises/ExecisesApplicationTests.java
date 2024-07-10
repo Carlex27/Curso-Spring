9b6cee39e17b2609210d692103a20967bec017ff
Carlex27 <carlosocampo2711@gmail.com>
2024-07-10 13:05:19 -0500

    Exercises project init

---
+++ Execises/src/test/java/com/example/Execises/ExecisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Execises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
