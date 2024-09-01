42adb3d122523780f8769f56a183fe05b34decaf
Carlos Ocampo <108969977+Carlex27@users.noreply.github.com>
2024-09-01 17:07:17 -0600

    Oauth2-Project

---
+++ Oauth2-Spring/src/test/java/com/example/Oauth2_/Spring/Oauth2SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Oauth2_.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
